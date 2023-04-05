1b3f1b91d781df1e000c6e21441e25a7ac9886b2
ieatn <ieatnubcake@gmail.com>
2023-04-05 11:11:10 -0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-
-type SquareValue = 'X' | 'O' | null;
-
-interface BoardProps {
-  squares: SquareValue[];
-  onClick: (i: number) => void;
-}
-
-interface SquareProps {
-  value: SquareValue;
-  onClick: () => void;
-}
-
-function Square(props: SquareProps) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-function Board(props: BoardProps) {
-  const renderSquare = (i: number) => {
-    return (
-      <Square
-        value={props.squares[i]}
-        onClick={() => props.onClick(i)}
-      />
-    );
-  }
-
-  return (
-    <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  );
-}
-
-function calculateWinner(squares: SquareValue[]): SquareValue {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+import Board from './container/Board';
 
 function App() {
-  const [squares, setSquares] = useState<SquareValue[]>(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
 
-  const handleClick = (i: number) => {
-    const newSquares = [...squares];
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-    newSquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  const winner = calculateWinner(squares);
-  let status: string;
-  if (winner) {
-    status = `Winner: ${winner}`;
-  } else {
-    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
-  const reset = () => {
-    setSquares(Array(9).fill(null));
-    setXIsNext(true);
-  }
-
+ 
   return (
     <div className="App">
-      <Board squares={squares} onClick={handleClick} />
-      <div className="status">{status}</div>
-      <button className="reset" onClick={reset}>Reset</button>
+      <Board />
     </div>
   );
 }
